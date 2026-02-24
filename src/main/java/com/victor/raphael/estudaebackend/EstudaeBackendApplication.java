14418fa3b5853974e5ede7e115cea82227e1858d
Victor Raphael <victor.raphael@usto.re>
2026-02-24 12:34:18 -0300

    [START] First Project Commit.

---
+++ src/main/java/com/victor/raphael/estudaebackend/EstudaeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.victor.raphael.estudaebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudaeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstudaeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/victor/raphael/estudaebackend/EstudaeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.raphael.estudaebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudaeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
